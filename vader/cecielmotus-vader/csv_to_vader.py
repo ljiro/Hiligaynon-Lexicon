223243f6caf4377dc70352ad910f9726835506f3
Melmar Palacio <vandi.mion04@gmail.com>
2025-09-15 12:13:05 +0800

    extract vader scores from cleaned dataset

--- vader/cecielmotus-vader/csv_to_vader.py
+++ vader/cecielmotus-vader/csv_to_vader.py
@@ -3,8 +3,8 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from pathlib import Path
 
 # Input/output paths
-csv_input = Path(r"C:\Users\CODE CLASSES\nlp\Hiligaynon-Lexicon\output\output_parsed.csv")
-lexicon_output = Path(r"C:\Users\CODE CLASSES\nlp\Hiligaynon-Lexicon\output\vader_hiligaynon_lexicon.txt")
+csv_input = Path(r"C:\Users\CODE CLASSES\nlp\Hiligaynon-Lexicon\preprocessed\Lexicon_merged.csv")
+lexicon_output = Path(r"C:\Users\CODE CLASSES\nlp\Hiligaynon-Lexicon\output\vader_hiligaynon_cleaned.txt")
 
 # Load the parsed CSV
 df = pd.read_csv(csv_input)
